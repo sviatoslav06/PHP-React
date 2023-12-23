402f2656d6688213788eb376a223f033a763093e
Святослав Самолюк <samoliuk.sviatoslav@gmail.com>
2023-12-23 20:27:20 +0200

    Initial

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\API\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get("categories",[CategoryController::class, 'getAll']);
+Route::post("categories/create",[CategoryController::class, 'create']);
+Route::get('/categories/{id}', [CategoryController::class, 'getById']);
+Route::delete("/categories/{id}", [CategoryController::class, "delete"]);
+Route::post("/categories/edit/{id}", [CategoryController::class, "edit"]);
